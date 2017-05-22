1ecc6db44d8d52898d55fda0f7a856f1f496c4ba
Vladimir <ostrovok@gmail.com>
2017-05-22 11:38:00 -0400

    Update sync.py

--- scripts/sync.py
+++ scripts/sync.py
@@ -7,6 +7,6 @@ import os
 54.213.140.254 172.31.28.45 spark.ostrovok.cc
 '''
 
-os.system('rsync -Pav -e "ssh -i ~/.ssh/my_keys.pem" /Users/ostrovok/test-tasks/climacell.co/* ubuntu@kafka.ostrovok.cc:/home/ubuntu/climacell/')
-os.system('rsync -Pav -e "ssh -i ~/.ssh/my_keys.pem" /Users/ostrovok/test-tasks/climacell.co/* ubuntu@spark.ostrovok.cc:/home/ubuntu/climacell/')
+os.system('rsync -Pav -e "ssh -i ~/.ssh/my_keys.pem" /Users/ostrovok/test-tasks/climatcell.co/* ubuntu@kafka.ostrovok.cc:/home/ubuntu/climatcell/')
+os.system('rsync -Pav -e "ssh -i ~/.ssh/my_keys.pem" /Users/ostrovok/test-tasks/climatcell.co/* ubuntu@spark.ostrovok.cc:/home/ubuntu/climatcell/')
 
